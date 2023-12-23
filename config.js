a6f85f60b5fa9ceeffca53508260a3af03088b57
Orzusaaaa <135080630+Orzusaaaa@users.noreply.github.com>
2023-12-23 17:35:21 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -2,15 +2,16 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['5217294888993'], ['573155428601'], ['5214531287294'], ['5492266466080'], ['593968585383'], ['573026191480'], ['5492266613038'], ['573106040746']]
+global.owner = [['56927170877', 'CuriosityBot-MD 🍧', true],
+['56927170877'], ['56927170877'], ['56927170877'], ['56927170877'], ['56927170877'], ['56927170877'], ['56927170877'], ['56927170877']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
 global.botNumberCode = '' //Ejemplo: +59309090909
 global.confirmCode = ''
 
-global.animxscans = ['5214531287294']
-global.suittag = ['5214531287294']
+global.animxscans = ['56927170877']
+global.suittag = ['56927170877']
 global.mods = []
 global.prems = []
 
